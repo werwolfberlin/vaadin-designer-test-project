201a1191eb3f46236e664312d5c7bd88d9f8ba13
Gerald Koch <gerald@vaadin.com>
2020-01-28 17:38:35 +0100

    created customized textfield and button

---
+++ src/main/java/org/vaadin/prevo/test/components/PTextField.java
@@ -0,0 +1,8 @@
+package org.vaadin.prevo.test.components;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.textfield.TextField;
+
+@Tag("p-text-field")
+public class PTextField extends TextField {
+}
